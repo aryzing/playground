1dad768581af565da77307e6d3b6a833c17b578d
Eduard Bardají Puig <ebardaji@gmail.com>
2016-04-11 11:29:44 +0200

    negative numbers are true

---
+++ tests/false-numbers/script.js
@@ -0,0 +1,14 @@
+// Zero (0) is the only flase number
+
+if (2) {
+  console.log("Two is true")
+}
+if (0) {
+  console.log("Zero is true")
+}
+if (-1) {
+  console.log("-1 is true")
+}
+if (-5) {
+  console.log("-5 is true")
+}
