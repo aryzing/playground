4ff338ce016cf5930cbfffa2497287d0c52e9551
Eduard Bardají Puig <ebardaji@gmail.com>
2016-05-09 21:59:43 -0700

    created ignore rules

--- tests/floating/script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(event) {
-  // code using jQuery here
-});
--- tests/grid-container-padding/test.js
+++
@@ -1,7 +0,0 @@
-var makeStopwatch = function() {
-  var elapsed = 0;
-  var stopwatch = function() {return elapsed;};
-  var increase = function() {elapsed++;};
-  setInterval(increase, 5000);
-  return stopwatch;
-};
